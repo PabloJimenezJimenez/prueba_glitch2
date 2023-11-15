7bf0b69c4d681833593bed476bc2a747bb74a760
Pablo Jimenez <Pablo.jimenez2@live.u-tad.com>
2023-11-15 10:24:20 +0100

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tienda_bootstrapAngular';
+  constructor(private el: ElementRef) { }
+
+  ngOnInit() {
+    this.navbarShrink();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.navbarShrink();
+  }
+
+  navbarShrink() {
+    const navbarCollapsible = this.el.nativeElement.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink');
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink');
+    }
+  }
 }
